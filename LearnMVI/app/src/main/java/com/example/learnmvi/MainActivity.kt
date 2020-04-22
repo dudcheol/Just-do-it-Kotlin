a143a8994819abb1d6e5d847c8ab948c87cd6487
dudcheol <parkyc0301@gmail.com>
2020-04-22 22:14:00 +0900

    Revert "mvi 스터디 - but... 내용 부족함"
    This reverts commit 044ffce477cfb731c317376cf61550f8b47431c0.

--- LearnMVI/app/src/main/java/com/example/learnmvi/MainActivity.kt
+++ LearnMVI/app/src/main/java/com/example/learnmvi/MainActivity.kt
@@ -1,37 +1,12 @@
 package com.example.learnmvi
 
-import android.os.Build
-import android.os.Bundle
-import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
-import com.jakewharton.rxbinding2.view.RxView
-import com.jakewharton.rxbinding2.widget.RxTextView
-import io.reactivex.Observable
-import kotlinx.android.synthetic.main.activity_main.*
-import java.util.function.Consumer
+import android.os.Bundle
 
-class MainActivity : AppCompatActivity(), MainView {
+class MainActivity : AppCompatActivity() {
 
-    @RequiresApi(Build.VERSION_CODES.N)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        MainIntent(this).start()
-    }
-
-    override fun getActions(): Map<String, Observable<Any>> {
-        val actions = hashMapOf<String, Observable<Any>>()
-        actions.put("Button", RxView.clicks(button))
-        actions.put("EditText", RxTextView.textChanges(editText) as Observable<Any>)
-        return actions
-    }
-
-    @RequiresApi(Build.VERSION_CODES.N)
-    override fun getConsumers(): Map<String, Consumer<Any>> {
-        val consumers = hashMapOf<String, Consumer<Any>>()
-        consumers.put("TextView", RxTextView.text(textView) as Consumer<Any>)
-        consumers.put("EditText", RxTextView.text(editText) as Consumer<Any>)
-        return consumers
     }
 }
